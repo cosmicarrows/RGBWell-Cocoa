8be68b9ad481b453aae6b29ecbf3a09ab2308728
Laurence Wingo <cosmicarrowsllc@cosmics-macbook-pro.local>
2018-05-03 12:43:46 -0400

    Implement the RGB Well UI from Scratch
    The reasons for starting from scratch is to show how they are all connected through well commented code.  Follow along.

---
+++ RGBWell/MainWindowController.swift
@@ -0,0 +1,14 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by Cosmic Arrows, LLC on 5/3/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    
+    
+}
