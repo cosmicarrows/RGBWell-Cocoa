8e6c88e0bb297253749bb4e9c26c72f58b285af1
Laurence Wingo <cosmicarrowsllc@cosmics-macbook-pro.local>
2018-05-04 12:21:01 -0400

    Implemented the NSColor Object within a Function for NSColorWell's Color Property
    The reasons for this is because NSColorWell's property is of type NSColor in which NSColor is initialized by three CGFloat objects.  Once we programmed the logic for the initializer based on the NSSlider's doubleValue property, we created a method called updateColor() and inside that method we set the ColorWell color property by setting it to the initialized NSColor.

--- RGBWell/MainWindowController.swift
+++ RGBWell/MainWindowController.swift
@@ -10,20 +10,42 @@ import Cocoa
 
 class MainWindowController: NSWindowController {
     
+    var r = 0.0
+    var g = 0.0
+    var b = 0.0
+    let a = 1.0
+    
+    @IBOutlet weak var colorWell: NSColorWell!
+    
     override var windowNibName: NSNib.Name? {
         return NSNib.Name("MainWindowController")
     }
     
     @IBAction func adjustRed(sender: NSSlider) {
         print("R slider's value is \(sender.floatValue)")
+        r = sender.doubleValue
+        updateColor()
     }
     
     @IBAction func adjustGreen(sender: NSSlider) {
         print("G slider's value is \(sender.floatValue)")
+        g = sender.doubleValue
+        updateColor()
     }
     
     @IBAction func adjustBlue(sender: NSSlider) {
         print("B slider's value is \(sender.floatValue)")
+        b = sender.doubleValue
+        updateColor()
+    }
+    
+    func updateColor() {
+        let newColor = NSColor.init(calibratedRed: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: CGFloat(a))
+        colorWell.color = newColor
+        
+        //the part that is a little confusing is the use of the updateColor() method being placed in the IBAction methods.....but wait so basically each time we have a target-action fire off, then it will call updateColor() which is an intitializer for an NSColor and it will update the ColorWell's color property which is of type NSColor!
+        
+        //we just initialized an object within a function and set a global property's variable which is a class so its a reference! NICE!  No additional memory was created during the making of this tutorial nor were there any animals harmed in the making of the color!
     }
     
 }
